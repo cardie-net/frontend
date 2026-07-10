845b7c3e8e9510bd15604c54a4abcdda079d704f
Artemii Kravchuk <mail@artiekra.org>
2026-07-10 16:28:52 +0300

    app boilerplate

---
+++ src/app/decks/page.tsx
@@ -0,0 +1,8 @@
+export default function Decks() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Decks Page</h1>
+      <p>Your decks will appear here.</p>
+    </main>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Home Page</h1>
+      <p>Welcome to the home page.</p>
+    </main>
+  );
+}
