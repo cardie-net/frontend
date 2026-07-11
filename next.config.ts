88ef0c5e84404ca9cd2e920c851ebac3af7411ba
Artemii Kravchuk <mail@artiekra.org>
2026-07-11 16:32:16 +0300

    rewrite api path when developing locally

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    // only proxy to localhost:8000 when developing locally
+    if (process.env.NODE_ENV === 'development') {
+      return [
+        {
+          source: '/api/:path*',
+          // The backend expects /api/v1/... because of the added prefix
+          destination: 'http://localhost:8000/api/:path*',
+        },
+      ];
+    }
+    return [];
+  },
 };
 
 export default nextConfig;
