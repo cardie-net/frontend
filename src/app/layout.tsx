a1f47460386c88bbdebcf3da24f6eefefe285340
Artemii Kravchuk <mail@artiekra.org>
2026-07-13 16:45:24 +0300

    use golos text font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Golos_Text } from 'next/font/google';
 import Link from 'next/link';
 import './globals.css';
 import { NavBar } from '@/components/NavBar';
 import { ThemeProvider } from '@/components/ThemeProvider';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const golosText = Golos_Text({
+  variable: '--font-golos-text',
   subsets: ['latin'],
 });
 
@@ -28,7 +23,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable}`}
+      className={`${golosText.variable} ${golosText.className}`}
       suppressHydrationWarning
     >
       <body>
